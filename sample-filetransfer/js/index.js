6dfa20c719630bb55fa2659a2f333fe48e141a98
Kristian Dimitrov <kristian.dimitrov.sz@gmail.com>
2013-05-31 10:54:25 -0500

    Rework file transfer sample to have a download button and field to display the path of the downloaded file.

--- sample-filetransfer/js/index.js
+++ sample-filetransfer/js/index.js
@@ -1,39 +1,73 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-    navigator.splashscreen.hide();
-    getFilesystem(
-        function(fileSystem) {
-            console.log("success");
-            transferFile(fileSystem.root.fullPath);
-        },
-        function() {
-            console.log("failed to get filesystem");
-        }
-    );
+	var that = this,
+	App = new downloadApp(),
+	fileName = "sample.png",
+	uri = encodeURI("http://www.icenium.com/assets/img/icenium-logo.png"),
+    folderName = "test";
+    
+	navigator.splashscreen.hide();
+	App.run(uri, fileName, folderName);
 }
 
-function getFilesystem(success, fail){
-    window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, success, fail);
+var downloadApp = function() {
 }
 
-function transferFile(fileSystemPath){
-    var transfer = new FileTransfer(),
-    uri = encodeURI("http://icenium.com/assets/img/icenium-logo.png");
-    filePath = fileSystemPath+'/sample.png';
-    transfer.download(
-        uri,
-        filePath,
-        function(entry) {
-            var image = document.getElementById("result");
-            image.src=entry.fullPath;
-        },
-        function(error) {
-            console.log("download error source " + error.source);
-            console.log("download error target " + error.target);
-            console.log("upload error code" + error.code);
-        }
-    );
-}
+downloadApp.prototype = {
+	run: function(uri, fileName, folderName) {
+		var that = this,
+		filePath = "";
+        
+		document.getElementById("download").addEventListener("click", function() {
+			that.getFilesystem(
+				function(fileSystem) {
+					console.log("gotFS");
+                    
+					if (device.platform === "Android") {
+						that.getFolder(fileSystem, folderName, function(folder) {
+							filePath = folder.fullPath + "\/" + fileName;
+							that.transferFile(uri, filePath)
+						}, function() {
+							console.log("failed to get folder");
+						});
+					}
+					else {
+						filePath = fileSystem.root.fullPath + "\/" + fileName;
+						that.transferFile(uri, filePath)
+					}
+				},
+				function() {
+					console.log("failed to get filesystem");
+				}
+				);
+		});
+	},
+    
+	getFilesystem:function (success, fail) {
+		window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, success, fail);
+	},
 
+	getFolder: function (fileSystem, folderName, success, fail) {
+		fileSystem.root.getDirectory(folderName, {create: true, exclusive: false}, success, fail)
+	},
+
+	transferFile: function (uri, filePath) {
+		var transfer = new FileTransfer();
+		transfer.download(
+			uri,
+			filePath,
+			function(entry) {
+				var image = document.getElementById("downloadedImage");
+				image.src = entry.fullPath;
+				document.getElementById("result").innerHTML = "File saved to: " + entry.fullPath;
+			},
+			function(error) {
+				console.log("download error source " + error.source);
+				console.log("download error target " + error.target);
+				console.log("upload error code" + error.code);
+			}
+			);
+	}
+}
